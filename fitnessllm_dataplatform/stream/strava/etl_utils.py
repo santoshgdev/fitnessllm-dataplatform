d7046db0b8ec766909b2d5ea10453dfb297ebbcc
Santosh Gummidipundi <santosh@santoshg.io>
2025-02-01 17:17:42 -0800

    refactored

--- fitnessllm_dataplatform/stream/strava/etl_utils.py
+++ fitnessllm_dataplatform/stream/strava/etl_utils.py
@@ -18,7 +18,8 @@ from tqdm_joblib import tqdm_joblib
 from fitnessllm_dataplatform.entities.dataclasses import Metrics
 from fitnessllm_dataplatform.entities.enums import (
     FitnessLLMDataSource,
-    FitnessLLMDataStream, Status,
+    FitnessLLMDataStream,
+    Status,
 )
 from fitnessllm_dataplatform.entities.queries import get_activities
 from fitnessllm_dataplatform.stream.strava.cloud_utils import get_strava_storage_path
@@ -26,81 +27,147 @@ from fitnessllm_dataplatform.stream.strava.entities.enums import StravaStreams
 from fitnessllm_dataplatform.utils.logging_utils import logger
 
 
-def convert_stream_json_to_dataframe(stream_json):
+def upsert_to_bigquery(
+    client: bigquery.Client,
+    athlete_id: str,
+    stream: StravaStreams,
+    dataframes: list[pd.DataFrame],
+    metrics: list[dict],
+):
+    timestamp = datetime.now()
+    try:
+        client.load_table_from_dataframe(
+            dataframe=dataframes,
+            destination=f"dev_strava.{stream}",
+            job_config=bigquery.LoadJobConfig(
+                write_disposition=bigquery.WriteDisposition.WRITE_APPEND
+            ),
+        )
+        insert_metrics(
+            metrics_list=metrics,
+            destination="dev_metrics.metrics",
+            timestamp=timestamp,
+            status=Status.SUCCESS,
+        )
+    except Exception as e:
+        logger.error(f"Error while inserting for {stream.value} for {athlete_id}: {e}")
+        insert_metrics(
+            metrics_list=metrics,
+            destination=f"dev_metrics.metrics",
+            timestamp=timestamp,
+            status=Status.FAILURE,
+        )
 
 
-def load_json_into_bq(InfrastructureNames: Enum, athlete_id: str, data_streams: list[str] | None):
+def convert_stream_json_to_dataframe(
+    stream: StravaStreams,
+    client: bigquery.Client,
+    athlete_id: str,
+    partial_strava_storage,
+):
+    partial_load_json_into_dataframe = partial(
+        load_json_into_dataframe,
+        athlete_id=athlete_id,
+        data_source=FitnessLLMDataSource.STRAVA,
+    )
+
+    activity_ids = (
+        client.query(
+            get_activities(
+                athlete_id=athlete_id,
+                data_source=FitnessLLMDataSource.STRAVA,
+                data_stream=stream,
+            )
+        )
+        .to_dataframe()["activity_id"]
+        .values
+    )
+
+    module_strava_json_list = (
+        list(
+            itertools.islice(
+                partial_strava_storage(strava_model=stream).iterdir(),
+                int(environ["SAMPLE"]),
+            )
+        )
+        if environ.get("SAMPLE")
+        else list(partial_strava_storage(strava_model=stream).iterdir())
+    )
+    filtered_module_strava_json_list = [
+        file
+        for file in module_strava_json_list
+        if file.stem.split("=")[1] not in activity_ids
+    ]
+
+    with tqdm_joblib(
+        tqdm(desc=f"Processing {stream}", total=len(filtered_module_strava_json_list))
+    ):
+        result = Parallel(n_jobs=1, backend="threading")(
+            delayed(partial_load_json_into_dataframe)(
+                file=json_file, data_stream=stream
+            )
+            for json_file in filtered_module_strava_json_list
+        )
+
+    dataframes = [result["dataframe"] for result in result]
+    metrics = [result["metrics"] for result in result]
+    return dataframes, metrics
+
+
+def load_json_into_bq(
+    InfrastructureNames: Enum, athlete_id: str, data_streams: list[str] | None
+):
     partial_strava_storage = partial(
         get_strava_storage_path,
         bucket=InfrastructureNames.bronze_bucket,
         athlete_id=athlete_id,
     )
 
-    partial_load_json_into_dataframe = partial(
-        load_json_into_dataframe,
-        athlete_id=athlete_id,
-        data_source=FitnessLLMDataSource.STRAVA,
-    )
     client = bigquery.Client()
 
     streams = [
-        element.name for element in partial_strava_storage(strava_model=None).iterdir()
+        StravaStreams[element.name.upper()]
+        for element in partial_strava_storage(strava_model=None).iterdir()
     ]
     if data_streams:
-        streams = [stream for stream in streams if stream in data_streams]
+        streams = [stream for stream in streams if stream.value in data_streams]
 
     for stream in streams:
-        stream_enum = StravaStreams[stream.upper()]
-        activity_ids = client.query(get_activities(athlete_id=athlete_id,
-                                                   data_source=FitnessLLMDataSource.STRAVA,
-                                                   data_stream=stream_enum)).to_dataframe()['activity_id'].values
-
-        module_strava_json_list = list(itertools.islice(partial_strava_storage(strava_model=stream_enum).iterdir(), int(environ['SAMPLE']))) if environ.get('SAMPLE') else list(partial_strava_storage(strava_model=stream_enum).iterdir())
-        filtered_module_strava_json_list = [file for file in module_strava_json_list if file.stem.split("=")[1] not in activity_ids]
-
-        with tqdm_joblib(tqdm(desc=f"Processing {stream}", total=len(filtered_module_strava_json_list))):
-            result = Parallel(n_jobs=1, backend="threading")(
-                delayed(partial_load_json_into_dataframe)(file=json_file, data_stream=stream)
-                for json_file in filtered_module_strava_json_list
-            )
-
-        dataframes = pd.concat([result["dataframe"] for result in result])
-        metrics = [result["metrics"] for result in result]
-        timestamp = datetime.now()
-
-        try:
-            client.load_table_from_dataframe(dataframe=dataframes,
-                                             destination=f"dev_strava.{stream}",
-                                             job_config=bigquery.LoadJobConfig(write_disposition=bigquery.WriteDisposition.WRITE_APPEND))
-            insert_metrics(metrics_list=metrics,
-                           destination="dev_metrics.metrics",
-                           timestamp=timestamp,
-                           status=Status.SUCCESS)
-        except Exception as e:
-            logger.error(f"Error while inserting for {stream_enum.value} for {athlete_id}: {e}")
-            insert_metrics(metrics_list=metrics,
-                           destination=f"dev_metrics.metrics",
-                           timestamp=timestamp,
-                           status=Status.FAILURE)
+        dataframes, metrics = convert_stream_json_to_dataframe(
+            stream=stream,
+            client=client,
+            athlete_id=athlete_id,
+            partial_strava_storage=partial_strava_storage,
+        )
+        upsert_to_bigquery(
+            client=client,
+            athlete_id=athlete_id,
+            stream=stream,
+            dataframes=dataframes,
+            metrics=metrics,
+        )
 
 
-
-def insert_metrics(metrics_list: list[Metrics],
-                   destination: str,
-                   timestamp: datetime,
-                   status: Status):
+def insert_metrics(
+    metrics_list: list[Metrics], destination: str, timestamp: datetime, status: Status
+):
     try:
         client = bigquery.Client()
-        metrics_list = [asdict(metrics.update(bq_insert_timestamp=timestamp, status=status.value)) for metrics in metrics_list]
+        metrics_list = [
+            asdict(metrics.update(bq_insert_timestamp=timestamp, status=status.value))
+            for metrics in metrics_list
+        ]
         dataframe = pd.DataFrame(metrics_list)
-        client.load_table_from_dataframe(dataframe=dataframe,
-                                         destination=destination,
-                                         job_config=bigquery.LoadJobConfig(write_disposition=bigquery.WriteDisposition.WRITE_APPEND))
+        client.load_table_from_dataframe(
+            dataframe=dataframe,
+            destination=destination,
+            job_config=bigquery.LoadJobConfig(
+                write_disposition=bigquery.WriteDisposition.WRITE_APPEND
+            ),
+        )
     except Exception as e:
         logger.error("Unable to write metrics to BigQuery.")
         raise e
-
-
 
 
 def process_json(input_dict: dict) -> dict:
@@ -129,24 +196,35 @@ def process_json(input_dict: dict) -> dict:
 
 def load_json_into_dataframe(
     athlete_id: str, file: GSPath, data_source, data_stream: FitnessLLMDataStream
-) -> dict[str, DataFrame| Metrics]:
+) -> dict[str, DataFrame | Metrics]:
     loaded_json = {
         "athlete_id": athlete_id,
         "activity_id": file.stem.split("=")[1],
         "data": json.loads(file.read_text()),
     }
     processed_jsons = process_json(loaded_json)
-    partial_metrics = partial(Metrics,
-                              athlete_id=athlete_id,
-                              activity_id=file.stem.split("=")[1],
-                              data_source=data_source.value,
-                              data_stream=data_stream)
+    partial_metrics = partial(
+        Metrics,
+        athlete_id=athlete_id,
+        activity_id=file.stem.split("=")[1],
+        data_source=data_source.value,
+        data_stream=data_stream,
+    )
 
     if isinstance(processed_jsons, list):
         if isinstance(processed_jsons[0], list):
             dataframe = DataFrame(list(itertools.chain(*processed_jsons)))
-            return {"dataframe": dataframe, "metrics": partial_metrics(record_count=dataframe.shape[0])}
+            return {
+                "dataframe": dataframe,
+                "metrics": partial_metrics(record_count=dataframe.shape[0]),
+            }
         dataframe = DataFrame(processed_jsons)
-        return {"dataframe": dataframe, "metrics": partial_metrics(record_count=dataframe.shape[0])}
+        return {
+            "dataframe": dataframe,
+            "metrics": partial_metrics(record_count=dataframe.shape[0]),
+        }
     dataframe = DataFrame(processed_jsons, index=[0])
-    return {"dataframe": dataframe, "metrics": partial_metrics(record_count=dataframe.shape[0])}
+    return {
+        "dataframe": dataframe,
+        "metrics": partial_metrics(record_count=dataframe.shape[0]),
+    }

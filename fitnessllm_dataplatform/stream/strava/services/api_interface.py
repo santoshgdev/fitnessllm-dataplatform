7c1895d2ec56510ed3673502712542bb18e20a27
Santosh Gummidipundi <santosh@santoshg.io>
2025-02-02 21:47:45 -0800

    black linter

--- fitnessllm_dataplatform/stream/strava/services/api_interface.py
+++ fitnessllm_dataplatform/stream/strava/services/api_interface.py
@@ -11,7 +11,11 @@ from tqdm import tqdm
 from fitnessllm_dataplatform.infrastructure.RedisConnect import RedisConnect
 from fitnessllm_dataplatform.services.api_interface import APIInterface
 from fitnessllm_dataplatform.stream.strava.cloud_utils import get_strava_storage_path
-from fitnessllm_dataplatform.stream.strava.entities.enums import StravaKeys, StravaURLs, StravaStreams
+from fitnessllm_dataplatform.stream.strava.entities.enums import (
+    StravaKeys,
+    StravaURLs,
+    StravaStreams,
+)
 from fitnessllm_dataplatform.utils.cloud_utils import get_secret, write_json_to_storage
 from fitnessllm_dataplatform.utils.logging_utils import logger
 from fitnessllm_dataplatform.utils.reques_utils import handle_status_code
@@ -19,21 +23,23 @@ from fitnessllm_dataplatform.utils.task_utils import get_enum_values_from_list
 
 
 class StravaAPIInterface(APIInterface):
-    def __init__(self, redis = None):
+    def __init__(self, redis=None):
         super().__init__()
         self.client = None
         self.redis = redis or RedisConnect()
         self.refresh_access_token_at_expiration()
-        strava_access_token_dict = self.redis.read_redis(StravaKeys.STRAVA_ACCESS_TOKEN.value)
+        strava_access_token_dict = self.redis.read_redis(
+            StravaKeys.STRAVA_ACCESS_TOKEN.value
+        )
         self.instantiate_strava_lib(strava_access_token_dict)
 
-
     @staticmethod
-    def get_strava_access_token(client_id: str,
-                                client_secret: str,
-                                code: str,
-                                grant_type: str = "authorization_code",
-                                ) -> dict:
+    def get_strava_access_token(
+        client_id: str,
+        client_secret: str,
+        code: str,
+        grant_type: str = "authorization_code",
+    ) -> dict:
         """Retrieve strava access token.
 
         Args:
@@ -82,8 +88,7 @@ class StravaAPIInterface(APIInterface):
             ttl=strava_access_token["expires_in"] - 60,
         )
         logger.info("Refreshed strava access token")
-    
-    
+
     def instantiate_strava_lib(self, strava_access_dict_from_redis: dict):
         """Instantiate strava client."""
         strava_access_token = (
@@ -94,8 +99,7 @@ class StravaAPIInterface(APIInterface):
         if not strava_access_token:
             return None
         self.client = Client(access_token=strava_access_token)
-    
-    
+
     def get_all_data(self, InfrastructureNames: Enum) -> str | None:
         """Get all activities."""
         athlete = self.client.get_athlete()
@@ -103,7 +107,7 @@ class StravaAPIInterface(APIInterface):
         partial_get_strava_storage = partial(
             get_strava_storage_path,
             bucket=InfrastructureNames.bronze_bucket,
-            athlete_id=athlete_id
+            athlete_id=athlete_id,
         )
 
         path = partial_get_strava_storage(strava_model=StravaStreams.ATHLETE_SUMMARY)
@@ -112,17 +116,26 @@ class StravaAPIInterface(APIInterface):
         activities = list(self.client.get_activities())
         for activity in tqdm(activities, desc="Getting activities"):
             activity_dump = activity.model_dump()
-            athlete_id, activity_id = activity_dump["athlete"]["id"], activity_dump["id"]
-            path = partial_get_strava_storage(strava_model=StravaStreams.ACTIVITY, activity_id=activity_id)
+            athlete_id, activity_id = (
+                activity_dump["athlete"]["id"],
+                activity_dump["id"],
+            )
+            path = partial_get_strava_storage(
+                strava_model=StravaStreams.ACTIVITY, activity_id=activity_id
+            )
             write_json_to_storage(path, json.loads(activity.model_dump_json()))
 
-            non_activity_streams = StravaStreams.filter_streams(exclude=['ACTIVITY','ATHLETE_SUMMARY'])
+            non_activity_streams = StravaStreams.filter_streams(
+                exclude=["ACTIVITY", "ATHLETE_SUMMARY"]
+            )
             streams = self.client.get_activity_streams(
-                activity_id=activity_id, types=get_enum_values_from_list(non_activity_streams)
+                activity_id=activity_id,
+                types=get_enum_values_from_list(non_activity_streams),
             )
             for stream in non_activity_streams:
                 stream_data = streams.get(stream.value, Stream())
-                path = partial_get_strava_storage(strava_model=stream, activity_id=activity_id)
+                path = partial_get_strava_storage(
+                    strava_model=stream, activity_id=activity_id
+                )
                 write_json_to_storage(path, stream_data.model_dump_json())
         return athlete_id
-

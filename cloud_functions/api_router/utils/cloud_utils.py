49991d91ed042d6c295b40c9e921aa7659ef8645
santoshgdev <santosh@santoshg.io>
2025-05-02 18:27:20 -0700

    Create cloud_utils.py

---
+++ cloud_functions/api_router/utils/cloud_utils.py
@@ -0,0 +1,8 @@
+import google.auth
+import google.auth.transport.requests
+
+def get_oauth_token() -> str:
+    credentials, _ = google.auth.default(scopes=["https://www.googleapis.com/auth/cloud-platform"])
+    auth_req = google.auth.transport.requests.Request()
+    credentials.refresh(auth_req)
+    return credentials.token

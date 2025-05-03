5fc3f687e8cdfaff78d48a189c0689593f12faf8
santoshgdev <santosh@santoshg.io>
2025-05-02 19:38:14 -0700

    Update cloud_utils.py

--- cloud_functions/api_router/utils/cloud_utils.py
+++ cloud_functions/api_router/utils/cloud_utils.py
@@ -1,8 +1,13 @@
+"""Cloud utils for api_router."""
 import google.auth
 import google.auth.transport.requests
 
+
 def get_oauth_token() -> str:
-    credentials, _ = google.auth.default(scopes=["https://www.googleapis.com/auth/cloud-platform"])
+    """Retrieve 0auth2 token."""
+    credentials, _ = google.auth.default(
+        scopes=["https://www.googleapis.com/auth/cloud-platform"]
+    )
     auth_req = google.auth.transport.requests.Request()
     credentials.refresh(auth_req)
     return credentials.token

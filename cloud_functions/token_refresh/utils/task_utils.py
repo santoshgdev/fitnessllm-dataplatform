130dfe22ae8b8aef26854148731c93b9fbc1d959
Santosh Gummidipundi <santosh@santoshg.io>
2025-05-10 13:51:26 -0700

    fixed pytz

--- cloud_functions/token_refresh/utils/task_utils.py
+++ cloud_functions/token_refresh/utils/task_utils.py
@@ -1,9 +1,8 @@
 """Task utils."""
 from datetime import datetime
-from zoneinfo import ZoneInfo
 
-def update_last_refresh() -> datetime:
-    return datetime.now(ZoneInfo("America/Los_Angeles"))
+import pytz
+
 
 def update_last_refresh() -> datetime:
     """Returns the current date and time in the America/Los_Angeles timezone.

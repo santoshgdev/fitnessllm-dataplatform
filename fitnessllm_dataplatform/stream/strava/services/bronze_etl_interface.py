f0b233096bf36cd181ca3bb814a52f981a61224f
Santosh Gummidipundi <santosh@santoshg.io>
2025-05-11 18:35:13 -0700

    update

--- fitnessllm_dataplatform/stream/strava/services/bronze_etl_interface.py
+++ fitnessllm_dataplatform/stream/strava/services/bronze_etl_interface.py
@@ -99,13 +99,6 @@ class BronzeStravaETLInterface(ETLInterface):
                     metrics=metrics,
                 )
             else:
-                print(
-                    "Logger handlers:", structured_logger.logger.handlers
-                )  # TODO: Delete this
-                print(
-                    "GCP Authenticated:",
-                    getattr(structured_logger, "authenticated", None),
-                )  # TODO: Delete this
                 structured_logger.warning(
                     message="No new data",
                     uid=self.uid,
